3c53135b6d41206f39c7e3ad5dbef7fc03b33587
amazbhola <amazingbhola@gmail.com>
2021-04-04 10:22:37 +0600

    Admin LTE Template Configure with npm

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('');
 });
 
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
-    return view('dashboard');
+    return view('admin.index');
 })->name('dashboard');
